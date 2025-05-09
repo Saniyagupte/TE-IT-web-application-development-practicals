888a4c73428fd22089bcae4718c8f92ff9654c31
Saniyagupte <wordlesswebsite@gmail.com>
2025-05-09 08:08:38 +0530

    first commit

---
+++ 15/server.js
@@ -0,0 +1,15 @@
+const express=require('express')
+const path=require('path')
+
+const app=express();
+
+app.use(express.static('public'))
+
+app.get('/', (req,res)=>{
+    res.sendFile(path.join(__dirname , 'static' , 'index.html'))
+
+})
+
+app.listen(3000 , ()=>{
+    console.log("listening on 3000");
+})
